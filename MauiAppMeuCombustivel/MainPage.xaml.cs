a7fe733018eb531c680ef8783312959100af5e09
jean1 <jean1@NOTEDELL>
2024-11-14 18:02:01 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiAppMeuCombustivel
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Obtém os valores inseridos para etanol e gasolina
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                // Obtém os valores da marca e modelo do veículo
+                string marca = txt_marca_do_veiculo.Text;
+                string modelo = txt_modelo_do_veiculo.Text;
+
+                string msg = "";
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = $"O etanol está compensando para o seu {marca} {modelo}.";
+                }
+                else
+                {
+                    msg = $"A gasolina está compensando para o seu {marca} {modelo}.";
+                }
+
+                // Exibe a mensagem
+                DisplayAlert("Calculado", msg, "OK");
+
+            }
+            catch (Exception ex)
+            {
+                // Exibe a mensagem de erro caso ocorra algum problema
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+        } // Fecha método
+    } // Fecha Class
+} // Fecha namespace
